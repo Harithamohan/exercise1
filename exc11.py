1f37f6b645e5f6005a7762890c904bf21fd96176
Harithamohan <36687339+Harithamohan@users.noreply.github.com>
2018-03-14 16:23:43 +0530

    Add files via upload

---
+++ exc11.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+#set -x
+
+#This is the program to perform basic arithmetic operations 
+x = int(raw_input('enter first value'))
+y = int(raw_input('enter second value'))
+
+print "div is %d" %(x / y) 
+print "mul is %d" %(x * y)
+print "sub is %d" %(x - y)
+print "add is %d" %(x + y)
+
+
+
