0589702856019b40510de934865089a5fe1d1eb2
Harithamohan <36687339+Harithamohan@users.noreply.github.com>
2018-03-14 17:58:09 +0530

    Add files via upload

---
+++ exc12.py
@@ -0,0 +1,12 @@
+#This is the program to print  user input in different data types
+#! /usr/bin/env python
+
+value = input("enter the value")
+
+print ("integer value is " + str(int(value)))
+
+print ("float value is " + str(float(value)))
+
+print ("string value is " + str(str(value)))
+
+print ("boolean value is " + str(bool(value)))
