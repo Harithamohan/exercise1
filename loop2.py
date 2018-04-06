0248215e98a2bd605d5e230f7b94e814bd307c75
Harithamohan <haritha.mohan@vvdntech.in>
2018-04-06 16:30:57 +0530

    loop2.py

---
+++ loop2.py
@@ -0,0 +1,18 @@
+
+#! /usr/bin/python3
+
+#this is the program to print the letters from gnu-linux is rule the world now
+
+
+
+for c in "gnu-linux is rule the world now":
+
+  
+  if ((c >= 'a' and c <= 'z')):
+    
+    print (c , end = ' ')
+
+
+print()
+
+
